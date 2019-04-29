7740ae971d939ce320d48136a71a0d69a85b3a76
Phuc Le <456061@student.saxion.nl>
2019-04-29 17:47:38 +0200

    Update dateTimeTest.java

--- SE/Date/tests/dateTimeTest.java
+++ SE/Date/tests/dateTimeTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import sourceDate.dateTime;
 import org.junit.Test;
 
@@ -8,14 +9,14 @@ public class dateTimeTest {
     @Test
     public void testAddDays1() { // test adding day with fix amount of incrementing to the local time
         Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH, 5);
+        cal.add(Calendar.DAY_OF_MONTH, 10);
         System.out.println("The day after increment is: " + cal.getTime());
     }
 
     @Test
     public void testSubDays1() { // test subtracting day with fix amount of decrementing to the local time
         Calendar cal = Calendar.getInstance();
-        cal.add(Calendar.DAY_OF_MONTH, -15);
+        cal.add(Calendar.DAY_OF_MONTH, -25);
         System.out.println("The day after increment is: " + cal.getTime());
     }
 
@@ -26,6 +27,7 @@ public class dateTimeTest {
         System.out.println("The day before increment is: 25-4-2019");
         dateTime addOrSubDays = myDate.addOrSubDays(amount);
         System.out.println("The day after increment is: " + addOrSubDays.getDay() + "-" + addOrSubDays.getMonth() + "-" + addOrSubDays.getYear());
+        assertEquals("The day after increment is: 4-5-2019","The day after increment is: " + addOrSubDays.getDay() + "-" + addOrSubDays.getMonth() + "-" + addOrSubDays.getYear());
     }
 
     @Test
@@ -35,5 +37,6 @@ public class dateTimeTest {
         System.out.println("The day before increment is: 25-4-2019");
         dateTime addOrSubDays = myDate.addOrSubDays(amount);
         System.out.println("The day after increment is: " + addOrSubDays.getDay() + "-" + addOrSubDays.getMonth() + "-" + addOrSubDays.getYear());
+        assertEquals("The day after increment is: 31-3-2019","The day after increment is: " + addOrSubDays.getDay() + "-" + addOrSubDays.getMonth() + "-" + addOrSubDays.getYear());
     }
 }
